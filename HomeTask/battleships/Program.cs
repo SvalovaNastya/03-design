15c10552b90166be4fd11e0c952fb8ca39e1cf5b
SvalovaNastya <Sun-7402@mail.ru>
2015-02-25 20:19:51 +0500

    Сделала начальные изменения. Перенесла пока что очевидные new в Program

--- HomeTask/battleships/Program.cs
+++ HomeTask/battleships/Program.cs
@@ -6,23 +6,31 @@ using System.Threading;
 
 namespace battleships
 {
-	public class Program
-	{
-		private static void Main(string[] args)
-		{
-			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-			if (args.Length == 0)
-			{
-				Console.WriteLine("Usage: {0} <ai.exe>", Process.GetCurrentProcess().ProcessName);
-				return;
-			}
-			var aiPath = args[0];
-			var settings = new Settings("settings.txt");
-			var tester = new AiTester(settings);
-			if (File.Exists(aiPath))
-				tester.TestSingleFile(aiPath);
-			else
-				Console.WriteLine("No AI exe-file " + aiPath);
-		}
-	}
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: {0} <ai.exe>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+            var aiPath = args[0];
+            var settings = new Settings("settings.txt");
+            var gen = new MapGenerator(settings, new Random(settings.RandomSeed));
+            var vis = new GameVisualizer();
+            var headersArray = new object[] { "AiName", "Mean", "Sigma", "Median", "Crashes", "Bad%", "Games", "Score" };
+            var monitor = new ProcessMonitor(TimeSpan.FromSeconds(settings.TimeLimitSeconds * settings.GamesCount), settings.MemoryLimit);
+
+            if (File.Exists(aiPath))
+            {
+                var ai = new Ai(aiPath, monitor);
+                var tester = new AiTester(settings, gen, vis, monitor, ai, headersArray);
+                tester.TestSingleFile(aiPath);
+            }
+            else
+                Console.WriteLine("No AI exe-file " + aiPath);
+        }
+    }
 }
